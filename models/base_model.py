b44b2bf7852e4adc2af9889d7a13b3ca3c4d0117
Bonziman <eloirdiwi@gmail.com>
2024-02-06 14:21:17 +0100

    now we can create a dict from an instance and an instance from a dict, then save(store) that object into a json file storage, and if needed we can call reload() to reload all the save d objects in that storage JSON file

--- models/base_model.py
+++ models/base_model.py
@@ -2,8 +2,7 @@
 """Defines the BaseModel class"""
 from uuid import uuid4
 from datetime import datetime
-
-
+import models
 class BaseModel:
     """The BaseModel for HBNB project."""
 
@@ -17,6 +16,16 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        
+        ignored_attr = ['__class__']
+        if len(kwargs) !=0:
+            for key, value in kwargs.items():
+                if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, tform))
+                elif key not in ignored_attr:
+                    setattr(self, key, value)
+        else:
+            models.storage.new(self)
 
     def __str__(self):
         """Return the formated string representation"""
@@ -29,6 +38,7 @@ class BaseModel:
     def save(self):
         """updates the updated_at with the current datetime."""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all k/v of __dict__ of
