cbb0b8a4519e38e50f6b45df458f5ae106dee81c
Bonziman <eloirdiwi@gmail.com>
2024-02-07 17:02:11 +0100

    added update command to ythe interpreter to update or add a new attribute to an instance based on class name and id

--- console.py
+++ console.py
@@ -129,6 +129,43 @@ class HBNBCommand(cmd.Cmd):
                         result.append(str(instance))
                         print(result)
 
+    def do_update(self, arg):
+        """Updates an instance based on class name and id b adding\
+        or updating attributes.
+        Usage: update <class> <id> <attribute name> "<attribute value>"
+        """
+        args = parse(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) == 1:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = "{}.{}".format(class_name, obj_id)
+        if len(args) == 2:
+            print("** attribute name missing **")
+            return
+        arg_name = args[2]
+        if len(args) == 3:
+            print("** value missing **")
+            return
+        arg_value = args[3]
+        if not storage.all()[key]:
+            print("** no instance found **")
+            return
+        else:
+            instance = storage.all()[key]
+            setattr(instance, arg_name, arg_value)
+            storage.save()
+        
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
