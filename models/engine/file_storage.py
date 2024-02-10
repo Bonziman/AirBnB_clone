1298c1d8fae5b9a3c1c9df595f63ad5a177ce5ee
Hamza abouabid <53376410+Mecreal@users.noreply.github.com>
2024-02-10 21:28:59 +0100

    Merge remote-tracking branch 'ayman/origin'

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Defines a FileStorage class."""
+import json
+from ..base_model import BaseModel
+from ..user import User
+
+
+class FileStorage:
+    """Handles serialization and deserialization of instances
+    to/from JSON file."""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id
+        Args:
+            obj: the obj to set in __objects
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objects to the JSON file(path:__file_path)"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(serialized_objects, f)
+
+    def reload(self):
+        """Deserializes the json file to __objects"""
+        try:
+            with open(self.__file_path, 'r') as file:
+                serialized_objects = json.load(file)
+                for key, value in serialized_objects.items():
+                    class_name, obj_id = key.split('.')
+                    self.__objects[key] = eval(class_name)(**value)
+        except FileNotFoundError:
+            pass
